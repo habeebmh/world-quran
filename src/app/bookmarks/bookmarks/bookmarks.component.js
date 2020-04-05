8b845ddc7ff4bd4dcb4e9529e04af36dfdf81de6
Habeeb Hooshmand <habeeb.hooshmand@gmail.com>
2020-04-04 19:35:36 -0500

    Added loading for bookmarks page.

--- src/app/bookmarks/bookmarks/bookmarks.component.js
+++ src/app/bookmarks/bookmarks/bookmarks.component.js
@@ -6,11 +6,13 @@ import QuranService from "../../shared/services/quran.service.js";
 import BookmarkService from "../bookmark.service.js";
 import Router from "../../router.js";
 import {Sura} from "../../shared/data/quran.data.js";
+import {ARABIC_TEXT} from "../../shared/constants/constants.js";
 
 export class BookmarksComponent extends LitElement {
   static get properties() {
     return {
       bookmarks: {type: Object},
+      loading: {type: Boolean},
       translation: {type: Object},
     };
   }
@@ -18,6 +20,7 @@ export class BookmarksComponent extends LitElement {
   constructor() {
     super();
     this.bookmarks = [];
+    this.loading = false;
     this.translation = {};
   }
 
@@ -34,21 +37,27 @@ export class BookmarksComponent extends LitElement {
         <quran-app-navigation bookmarks></quran-app-navigation>
     </div>
     <quran-body>
-    ${this.bookmarks.length > 0 ? '' : html`
-    <h2>No bookmarks</h2>
-    <h3>To Bookmark a verse click the <quran-material-icon icon="bookmark_border"></quran-material-icon> icon.</h3>
-    `}
-    ${this.bookmarks.map(([chapter, name, verse]) => html`
-      <quran-translated-text
-          arabictext="${verse.arabic}"
-          translationtext="${verse.translation}"
-          translationlang="${this.translation.language}">
-        <div slot="super">
-          ${verse.facts ? html`${verse.facts.sajda ? html`<sup>[سُجود]</sup>` : ''}` : ''}
-        </div>
-        <div slot="top-info"><h2>${name}</h2><h4><span class="link" @click="${() => this.goToChapter(chapter, verse.number)}">${chapter}:${verse.number}</span></h4></div>
-      </quran-translated-text>
-    `)}
+    ${this.loading
+      ? html`<paper-spinner active></paper-spinner>`
+      : html`
+      ${this.bookmarks.length === 0
+        ? html`
+        <h2>No bookmarks</h2>
+        <h3>To Bookmark a verse click the <quran-material-icon icon="bookmark_border"></quran-material-icon> icon.</h3>`
+        : html`
+        ${this.bookmarks.map(([chapter, name, verse]) => html`
+          <quran-translated-text
+              arabictext="${verse.arabic}"
+              translationtext="${verse.translation}"
+              translationlang="${this.translation.language}">
+            <div slot="super">
+              ${verse.facts ? html`${verse.facts.sajda ? html`<sup>[سُجود]</sup>` : ''}` : ''}
+            </div>
+            <div slot="top-info"><h2>${name}</h2><h4><span class="link" @click="${() => this.goToChapter(chapter, verse.number)}">${chapter}:${verse.number}</span></h4></div>
+          </quran-translated-text>
+        `)}`
+      }`
+    }
     </quran-body>
     `;
   }
@@ -59,18 +68,20 @@ export class BookmarksComponent extends LitElement {
   }
 
   async getBookmarkedVerses() {
+    this.loading = true;
     const bookmarks = [];
     for (const bookmark of BookmarkService.bookmarks) {
       const name = Sura[bookmark.chapter - 1][4];
       const verseInfo = this.getVerse(bookmark.chapter, bookmark.verse);
       bookmarks.push([bookmark.chapter, await name, await verseInfo]);
     }
+    this.loading = false;
     return bookmarks;
   }
 
   async getVerse(chapter, verse) {
     const queryTranslation = this.translation.id !== 'none';
-    const arabicXml = XmlService.readXML('quran-uthmani');
+    const arabicXml = XmlService.readXML(ARABIC_TEXT);
     const translationXml = queryTranslation ? XmlService.readXML(this.translation.id) : null;
     const facts = QuranService.getVerseMeta(chapter, verse);
 
@@ -83,7 +94,9 @@ export class BookmarksComponent extends LitElement {
   }
 
   getTextFromVerseNode(xml, chapter, verse) {
-    return xml.querySelector(`sura[index="${chapter}"]`).querySelector(`aya[index="${verse}"]`).getAttribute('text')
+    return xml.querySelector(`
+    sura[index = "${chapter}"]`).querySelector(`
+    aya[index = "${verse}"]`).getAttribute('text')
   }
 
   goToChapter(chapter, verse) {
